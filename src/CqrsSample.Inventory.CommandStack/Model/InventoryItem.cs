0e31ffd7a4a1ed888bc1dd28693024f3b2dfa6b6
enrico.massone@deltatre.it <enrico.massone@deltatre.it>
2018-12-16 00:33:21 +0100

    added xml docs to the stub implementation for method create new

--- src/CqrsSample.Inventory.CommandStack/Model/InventoryItem.cs
+++ src/CqrsSample.Inventory.CommandStack/Model/InventoryItem.cs
@@ -14,6 +14,13 @@ namespace CqrsSample.Inventory.CommandStack.Model
 
     public static class Factory
     {
+      /// <summary>
+      /// Creates a new inventory item
+      /// </summary>
+      /// <param name="id">The unique identifier of the inventory item. Cannot be the empty guid</param>
+      /// <param name="name">The name of the inventory item. Cannot be null or white space</param>
+      /// <returns>The newly created inventory item</returns>
+      /// <exception cref="ArgumentException">Throws <see cref="ArgumentException"/> when <paramref name="id"/> equals <see cref="Guid.Empty"/> or <paramref name="name"/> is null or white space</exception>
       public static InventoryItem CreateNew(Guid id, string name)
       {
         throw new NotImplementedException();
