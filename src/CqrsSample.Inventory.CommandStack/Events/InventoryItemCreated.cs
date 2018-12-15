571d5177f6c73ebf19f22b6ca78346c8ce34a3d2
enrico.massone@deltatre.it <enrico.massone@deltatre.it>
2018-12-16 00:15:41 +0100

    added an event for the creation of the inventory item aggregate

---
+++ src/CqrsSample.Inventory.CommandStack/Events/InventoryItemCreated.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CqrsSample.Inventory.CommandStack.Events
+{
+  /// <summary>
+  /// The domain event raised when an inventory item is created
+  /// </summary>
+  public class InventoryItemCreated : Event
+  {
+    public InventoryItemCreated(
+      Guid id,
+      string name,
+      int aggregateVersion) : base(aggregateVersion)
+    {
+      this.Id = id;
+      this.Name = name;
+    }
+
+    /// <summary>
+    /// Gets the unique identifier for the inventory item
+    /// </summary>
+    public Guid Id { get; }
+
+    /// <summary>
+    /// Gets the name of the inventory item
+    /// </summary>
+    public string Name { get; }
+  }
+}
