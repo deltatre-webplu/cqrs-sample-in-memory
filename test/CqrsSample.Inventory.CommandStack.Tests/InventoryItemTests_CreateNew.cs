dd10c30222844dde2d39104767b1747417fc4cf8
enrico.massone@deltatre.it <enrico.massone@deltatre.it>
2018-12-16 01:03:32 +0100

    got a complete test suite for create new method

---
+++ test/CqrsSample.Inventory.CommandStack.Tests/InventoryItemTests_CreateNew.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using CqrsSample.Inventory.CommandStack.Model;
+using NUnit.Framework;
+using System.Linq;
+using CqrsSample.Inventory.CommandStack.Events;
+
+namespace CqrsSample.Inventory.CommandStack.Tests.Model
+{
+  [TestFixture]
+  public partial class InventoryItemTests
+  {
+    [Test]
+    public void CreateNew_Throws_ArgumenException_When_Id_Is_Empty_Guid()
+    {
+      // ACT
+      Assert.Throws<ArgumentException>(
+        () => InventoryItem.Factory.CreateNew(Guid.Empty, "shoes")
+      );
+    }
+
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase("    ")]
+    public void CreateNew_Throws_ArgumenException_When_Name_Is_Null_Or_White_Space(string name)
+    {
+      // ACT
+      Assert.Throws<ArgumentException>(
+        () => InventoryItem.Factory.CreateNew(Guid.NewGuid(), name)
+      );
+    }
+
+    [Test]
+    public void CreateNew_Returns_New_Instance_With_Expected_Properties()
+    {
+      // ARRANGE
+      var id = Guid.NewGuid();
+      const string name = "tennis shoes";
+
+      // ACT
+      var result = InventoryItem.Factory.CreateNew(id, name);
+
+      // ASSERT
+      Assert.IsNotNull(result);
+      Assert.AreEqual(id, result.Id);
+      Assert.AreEqual(name, result.Name);
+    }
+
+    [Test]
+    public void CreateNew_Raises_InventoryItemCreated_Event()
+    {
+      // ARRANGE
+      var id = Guid.NewGuid();
+      const string name = "tennis shoes";
+
+      // ACT
+      var result = InventoryItem.Factory.CreateNew(id, name);
+
+      // ASSERT
+      var events = result.GetUncommittedChanges().ToArray();
+      Assert.AreEqual(1, events.Length);
+
+      var @event = events[0] as InventoryItemCreated;
+      Assert.IsNotNull(@event);
+      Assert.AreEqual(id, @event.Id);
+      Assert.AreEqual(name, @event.Name);
+    }
+  }
+}
