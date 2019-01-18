6915fa622847ba9d3330fc20063658fedfb76ccd
Enrico Massone <enrico.massone@deltatre.it>
2019-01-18 19:21:33 +0100

    defined event ItemsAddedToInventory

---
+++ src/CqrsSample.Inventory.CommandStack/Events/ItemsCheckedInToInventory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CqrsSample.Inventory.CommandStack.Events
+{
+  /// <summary>
+  /// The domain event raised when some items are added to the inventory
+  /// </summary>
+  public sealed class ItemsAddedToInventory : Event
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ItemsAddedToInventory"/> class.
+    /// </summary>
+    /// <param name="aggregateVersion">The aggregate version at the moment when the event was raised</param>
+    /// <param name="id">The unique identifier of the inventory item</param>
+    /// <param name="numberOfAddedItems">The number of items added to the inventory</param>
+    public ItemsAddedToInventory(
+      int aggregateVersion,
+      Guid id,
+      int numberOfAddedItems)
+      : base(aggregateVersion)
+    {
+      this.Id = id;
+      this.NumberOfAddedItems = numberOfAddedItems;
+    }
+
+    /// <summary>
+    /// Gets the unique identifier of the inventory item
+    /// </summary>
+    public Guid Id { get; }
+
+    /// <summary>
+    /// Gets the number of items added to the inventory
+    /// </summary>
+    public int NumberOfAddedItems { get; }
+  }
+}
