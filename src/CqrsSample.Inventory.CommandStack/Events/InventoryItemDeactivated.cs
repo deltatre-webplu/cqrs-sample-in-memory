6746f16b3f5128f4889f4f9840405ec6031c8437
Enrico Massone <enrico.massone@deltatre.it>
2019-01-18 18:21:55 +0100

    added test suite for method Deactivate

---
+++ src/CqrsSample.Inventory.CommandStack/Events/InventoryItemDeactivated.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CqrsSample.Inventory.CommandStack.Events
+{
+  /// <summary>
+  /// The domain event raised when an inventory item has been deactivated
+  /// </summary>
+  public sealed class InventoryItemDeactivated : Event
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InventoryItemDeactivated"/> class.
+    /// </summary>
+    /// <param name="aggregateVersion">The aggregate version at the moment when the event was raised</param>
+    /// <param name="id">The unique identifier of the inventory item</param>
+    public InventoryItemDeactivated(
+      int aggregateVersion,
+      Guid id) 
+      : base(aggregateVersion)
+    {
+      this.Id = id;
+    }
+
+    /// <summary>
+    /// Gets the unique identifier for the inventory item
+    /// </summary>
+    public Guid Id { get; }
+  }
+}
--- test/CqrsSample.Inventory.CommandStack.Tests/InventoryItemTests_Deactivate.cs
+++ test/CqrsSample.Inventory.CommandStack.Tests/InventoryItemTests_Deactivate.cs
@@ -1,12 +1,83 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using CqrsSample.Inventory.CommandStack.Events;
+using CqrsSample.Inventory.CommandStack.Model;
 using NUnit.Framework;
 
-namespace CqrsSample.Inventory.CommandStack.Tests
+namespace CqrsSample.Inventory.CommandStack.Tests.Model
 {
   public partial class InventoryItemTests
   {
+    [Test]
+    public void Deactivate_Raises_An_Event_Of_Type_InventoryItemDeactivated()
+    {
+      // ARRANGE
+      var id = Guid.NewGuid();
+      var target = InventoryItem.Factory.CreateNew(id, "tennis racket");
 
+      ((IAggregateRoot)target).MarkChangesAsCommitted();
+
+      // ACT
+      target.Deactivate();
+      var events = ((IAggregateRoot)target).GetUncommittedChanges().ToArray();
+
+      // ASSERT
+      Assert.IsNotNull(events);
+      Assert.AreEqual(1, events.Length);
+
+      var @event = events[0] as InventoryItemDeactivated;
+      Assert.IsNotNull(@event);
+      Assert.AreEqual(id, @event.Id);
+    }
+
+    [Test]
+    public void Deactivate_Changes_Aggregate_State()
+    {
+      // ARRANGE
+      var id = Guid.NewGuid();
+      var target = InventoryItem.Factory.CreateNew(id, "tennis racket");
+
+      // ACT
+      target.Deactivate();
+
+      // ASSERT
+      Assert.IsFalse(target.IsActive);
+    }
+
+    [Test]
+    public void Deactivate_Does_Not_Raise_Events_If_Inventory_Item_Is_Not_Active()
+    {
+      // ARRANGE
+      var id = Guid.NewGuid();
+      var target = InventoryItem.Factory.CreateNew(id, "tennis racket");
+
+      target.Deactivate();
+
+      ((IAggregateRoot)target).MarkChangesAsCommitted();
+
+      // ACT
+      target.Deactivate();
+      var events = ((IAggregateRoot)target).GetUncommittedChanges().ToArray();
+
+      // ASSERT
+      Assert.IsNotNull(events);
+      Assert.IsEmpty(events);
+    }
+
+    [Test]
+    public void Deactivate_Does_Not_Change_Aggregate_State_If_Inventory_Item_Is_Not_Active()
+    {
+      // ARRANGE
+      var id = Guid.NewGuid();
+      var target = InventoryItem.Factory.CreateNew(id, "tennis racket");
+
+      target.Deactivate();
+
+      // ACT
+      target.Deactivate();
+
+      // ASSERT
+      Assert.IsFalse(target.IsActive);
+    }
   }
 }
