d29434586a37190e39b449d6c885ddc2ed1e0fff
Enrico Massone <enrico.massone@deltatre.it>
2019-01-18 17:25:59 +0100

    added interface IAggregateRoot

---
+++ src/CqrsSample.Inventory.CommandStack/Model/IAggregateRoot.cs
@@ -0,0 +1,28 @@
+﻿using CqrsSample.Inventory.CommandStack.Events;
+using System.Collections.Generic;
+
+namespace CqrsSample.Inventory.CommandStack.Model
+{
+  /// <summary>
+  /// Interface modelling the behaviour of an aggregate root
+  /// </summary>
+  public interface IAggregateRoot
+  {
+    /// <summary>
+    /// Gets the uncommited aggregate changes
+    /// </summary>
+    /// <returns>A sequence containing all the uncommited aggregate changes</returns>
+    IEnumerable<Event> GetUncommittedChanges();
+
+    /// <summary>
+    /// Marks all uncommited aggregate changes as committed
+    /// </summary>
+    void MarkChangesAsCommitted();
+
+    /// <summary>
+    /// Rehydrate the aggregate from a stream of domain events
+    /// </summary>
+    /// <param name="history">The aggregate history as a stream of domain events</param>
+    void LoadsFromHistory(IEnumerable<Event> history);
+  }
+}
