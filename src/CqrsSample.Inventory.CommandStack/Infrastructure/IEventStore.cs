2f7b487aee4be3303b81070ecc6c8680cb9a74bb
enrico.massone@deltatre.it <enrico.massone@deltatre.it>
2019-03-02 00:35:30 +0100

    added interfaces for event store and repository

---
+++ src/CqrsSample.Inventory.CommandStack/Infrastructure/IEventStore.cs
@@ -0,0 +1,12 @@
+﻿using CqrsSample.Inventory.CommandStack.Events;
+using System;
+using System.Collections.Generic;
+
+namespace CqrsSample.Inventory.CommandStack.Infrastructure
+{
+  public interface IEventStore
+  {
+    void SaveEvents(Guid aggregateId, IEnumerable<Event> events);
+    IEnumerable<Event> GetEventsForAggregate(Guid aggregateId);
+  }
+}
