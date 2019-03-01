bc8b342cd7c55b0717a53cc68864f371736c4f21
enrico.massone@deltatre.it <enrico.massone@deltatre.it>
2019-03-02 00:37:27 +0100

    work in progress

---
+++ src/CqrsSample.Inventory.CommandStack/Infrastructure/Repository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CqrsSample.Inventory.CommandStack.Model;
+
+namespace CqrsSample.Inventory.CommandStack.Infrastructure
+{
+  public class Repository : IRepository
+  {
+    private readonly IEventStore eventStore;
+
+    public Repository(IEventStore eventStore)
+    {
+      this.eventStore = eventStore ?? throw new ArgumentNullException(nameof(eventStore));
+    }
+
+    public TAggregate GetById<TAggregate>(Guid aggregateId) where TAggregate : IAggregateRoot
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Save(IAggregateRoot aggregate, Guid aggregateId)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
