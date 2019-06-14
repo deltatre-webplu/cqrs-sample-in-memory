295ab72926eb9165a022f3ba3a2378af0ebeabe3
Enrico Massone <enrico.massone@deltatre.it>
2019-06-14 18:22:52 +0200

    Work in progress: creating a test suite for class Repository

---
+++ test/CqrsSample.Inventory.CommandStack.Tests/Infrastructure/RepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CqrsSample.Inventory.CommandStack.Model;
+using NUnit.Framework;
+
+namespace CqrsSample.Inventory.CommandStack.Tests.Infrastructure
+{
+  [TestFixture]
+  public class RepositoryTest
+  {
+
+    public class FakeAggregate : AggregateRoot
+    {
+      private FakeAggregate()
+      {
+      }
+
+      public string Name { get; private set; }
+      public int Age { get; private set; }
+    }
+  }
+}
