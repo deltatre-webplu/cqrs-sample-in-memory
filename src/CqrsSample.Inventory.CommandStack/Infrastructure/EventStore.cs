69ee13a426f7fdd8c081e463310f9bb8bc3842e7
Enrico Massone <enrico.massone@deltatre.it>
2019-07-26 18:39:15 +0200

    added xml docs

--- src/CqrsSample.Inventory.CommandStack/Infrastructure/EventStore.cs
+++ src/CqrsSample.Inventory.CommandStack/Infrastructure/EventStore.cs
@@ -4,6 +4,9 @@ using CqrsSample.Inventory.CommandStack.Events;
 
 namespace CqrsSample.Inventory.CommandStack.Infrastructure
 {
+  /// <summary>
+  /// An implementation of <see cref="IEventStore"/> interface based on NEventStore library 
+  /// </summary>
   public sealed class EventStore : IEventStore
   {
     public IEnumerable<Event> GetEventsForAggregate(Guid aggregateId)
