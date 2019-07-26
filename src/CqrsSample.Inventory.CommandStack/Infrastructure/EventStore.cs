54309eee01a293f4af118c4fac410b6a2ce0183d
Enrico Massone <enrico.massone@deltatre.it>
2019-07-26 18:37:48 +0200

    added NEventAStore and stub implementation of IEventStore interface

---
+++ src/CqrsSample.Inventory.CommandStack/Infrastructure/EventStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using CqrsSample.Inventory.CommandStack.Events;
+
+namespace CqrsSample.Inventory.CommandStack.Infrastructure
+{
+  public sealed class EventStore : IEventStore
+  {
+    public IEnumerable<Event> GetEventsForAggregate(Guid aggregateId)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void SaveEvents(Guid aggregateId, IEnumerable<Event> events, int expectedVersion)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
