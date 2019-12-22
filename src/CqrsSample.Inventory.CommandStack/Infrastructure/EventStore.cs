0220c5f9eb6caf75d7ee8f2b9ce8c9c50f7f0cf8
Enrico Massone <enrico.massone@deltatre.it>
2019-12-22 23:16:09 +0100

    changed interface definition

--- src/CqrsSample.Inventory.CommandStack/Infrastructure/EventStore.cs
+++ src/CqrsSample.Inventory.CommandStack/Infrastructure/EventStore.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using CqrsSample.Inventory.CommandStack.Events;
+using NEventStore;
 
 namespace CqrsSample.Inventory.CommandStack.Infrastructure
 {
   /// <summary>
-  /// An implementation of <see cref="IEventStore"/> interface based on NEventStore library 
+  /// An implementation of <see cref="IEventStore"/> interface based on the NEventStore library 
   /// </summary>
   public sealed class EventStore : IEventStore
   {
-    public IEnumerable<Event> GetEventsForAggregate(Guid aggregateId)
+    private readonly IStoreEvents _store;
+
+    public EventStore(IStoreEvents store)
+    {
+      _store = store ?? throw new ArgumentNullException(nameof(store));
+    }
+
+    public ReadOnlyCollection<Event> GetEventsForAggregate(Guid aggregateId)
     {
       throw new NotImplementedException();
     }
--- src/CqrsSample.Inventory.CommandStack/Infrastructure/IEventStore.cs
+++ src/CqrsSample.Inventory.CommandStack/Infrastructure/IEventStore.cs
@@ -1,6 +1,7 @@
 ﻿using CqrsSample.Inventory.CommandStack.Events;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace CqrsSample.Inventory.CommandStack.Infrastructure
 {
@@ -12,22 +13,37 @@ namespace CqrsSample.Inventory.CommandStack.Infrastructure
     /// <summary>
     /// Appends some events to the event stream of a specific aggregate.
     /// </summary>
-    /// <param name="aggregateId">The unique identifier of the aggregate for which the events must be appended to the event stream.</param>
-    /// <param name="events">The events to be appended to the aggregate's event stream.</param>
+    /// <param name="aggregateId">
+    /// The unique identifier of the aggregate for which the events must be appended to the event stream.
+    /// </param>
+    /// <param name="events">
+    /// The events to be appended to the aggregate's event stream.
+    /// </param>
     /// <param name="expectedVersion">
     /// The aggregate version being expected by the method caller.
-    /// This is used in order to perform the optimistic concurrency check before appending the new events to the event stream.
-    /// This parameter is expected to be a non negative integer. Pass the value zero (0) when you are creating a fresh new event stream.
+    /// This is used in order to perform the optimistic concurrency check before appending the new events 
+    /// to the event stream.
+    /// This parameter is expected to be a non negative integer. 
+    /// Pass the value zero (0) when you are creating a fresh new event stream.
     /// </param>
-    /// <exception cref="ArgumentNullException">Throws <see cref="ArgumentNullException"/> when parameter <paramref name="events"/> is null.</exception>
-    /// <exception cref="ArgumentOutOfRangeException">Throws <see cref="ArgumentOutOfRangeException"/> when parameter <paramref name="expectedVersion"/> is less than zero.</exception>
+    /// <exception cref="ArgumentNullException">
+    /// Throws <see cref="ArgumentNullException"/> when parameter <paramref name="events"/> is null.
+    /// </exception>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Throws <see cref="ArgumentOutOfRangeException"/> when parameter <paramref name="expectedVersion"/> 
+    /// is less than zero.
+    /// </exception>
     void SaveEvents(Guid aggregateId, IEnumerable<Event> events, int expectedVersion);
 
     /// <summary>
     /// Gets the full event stream for an aggregate. The returned event stream is sorted ascending by aggregate version. 
     /// </summary>
-    /// <param name="aggregateId">The unique identifier of the aggregate for which the event stream is returned.</param>
-    /// <returns>The full aggregate event stream sorted by aggregate version ascending.</returns>
-    IEnumerable<Event> GetEventsForAggregate(Guid aggregateId);
+    /// <param name="aggregateId">
+    /// The unique identifier of the aggregate for which the event stream is returned.
+    /// </param>
+    /// <returns>
+    /// The full aggregate event stream sorted by aggregate version ascending.
+    /// </returns>
+    ReadOnlyCollection<Event> GetEventsForAggregate(Guid aggregateId);
   }
 }
