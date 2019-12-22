411f266cbd8c63c333936f93ccc120e7222ac0be
Enrico Massone <enrico.massone@deltatre.it>
2019-12-23 00:20:49 +0100

    wip adding unit tests

---
+++ test/CqrsSample.Inventory.CommandStack.Tests/Infrastructure/EventStoreTests.cs
@@ -0,0 +1,34 @@
+﻿using CqrsSample.Inventory.CommandStack.Infrastructure;
+using Moq;
+using NEventStore;
+using NUnit.Framework;
+
+namespace CqrsSample.Inventory.CommandStack.Tests.Infrastructure
+{
+  [TestFixture]
+  public sealed class EventStoreTests
+  {
+    private Mock<IStoreEvents> _storeMock;
+
+    [SetUp]
+    public void Init() 
+    {
+      _storeMock = new Mock<IStoreEvents>();
+    }
+
+    [Test]
+    public void GetEventsForAggregate_Read_Events_From_Underlying_Event_Store() 
+    {
+      // ARRANGE
+      
+      // ACT
+
+      // ASSERT
+    }
+
+    private EventStore CreateTarget()
+    {
+      return new EventStore(_storeMock.Object);
+    }
+  }
+}
