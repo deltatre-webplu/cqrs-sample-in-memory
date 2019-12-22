9d1620ed9083eddb1f90ddd340e561c53b16e048
Enrico Massone <enrico.massone@deltatre.it>
2019-12-23 00:34:23 +0100

    addd first unit test for the read method on the event store

--- test/CqrsSample.Inventory.CommandStack.Tests/Infrastructure/EventStoreTests.cs
+++ test/CqrsSample.Inventory.CommandStack.Tests/Infrastructure/EventStoreTests.cs
@@ -1,7 +1,13 @@
-﻿using CqrsSample.Inventory.CommandStack.Infrastructure;
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using CqrsSample.Inventory.CommandStack.Events;
+using CqrsSample.Inventory.CommandStack.Infrastructure;
 using Moq;
 using NEventStore;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CqrsSample.Inventory.CommandStack.Tests.Infrastructure
 {
@@ -20,10 +26,36 @@ namespace CqrsSample.Inventory.CommandStack.Tests.Infrastructure
     public void GetEventsForAggregate_Read_Events_From_Underlying_Event_Store() 
     {
       // ARRANGE
-      
+      var fixture = new Fixture();
+      fixture.Customize(new AutoMoqCustomization());
+
+      var events = fixture.CreateMany<Event>(2);
+
+      var streamMock = new Mock<IEventStream>();
+      streamMock
+        .Setup(m => m.CommittedEvents)
+        .Returns(() => events.Select(e => new EventMessage { Body = e }).ToList());
+
+      _storeMock
+        .Setup(m =>
+          m.OpenStream(
+            It.IsAny<string>(),
+            It.IsAny<string>(),
+            It.IsAny<int>(),
+            It.IsAny<int>()
+          )
+        )
+        .Returns(streamMock.Object);
+
+      var target = this.CreateTarget();
+
       // ACT
+       var result = target.GetEventsForAggregate(Guid.NewGuid());
 
       // ASSERT
+      Assert.IsNotNull(result);
+      Assert.AreEqual(2, result.Count);
+      CollectionAssert.AreEqual(events, result);
     }
 
     private EventStore CreateTarget()
