ac5ab2d9f157ae7e84e987ef76ae864694e27cef
Enrico Massone <enrico.massone@deltatre.it>
2019-12-23 00:37:47 +0100

    WIP: adding unit test for the event store

--- test/CqrsSample.Inventory.CommandStack.Tests/Infrastructure/EventStoreTests.cs
+++ test/CqrsSample.Inventory.CommandStack.Tests/Infrastructure/EventStoreTests.cs
@@ -23,7 +23,7 @@ namespace CqrsSample.Inventory.CommandStack.Tests.Infrastructure
     }
 
     [Test]
-    public void GetEventsForAggregate_Read_Events_From_Underlying_Event_Store() 
+    public void GetEventsForAggregate_Returns_Events_Contained_In_Underlying_Event_Store()
     {
       // ARRANGE
       var fixture = new Fixture();
@@ -56,6 +56,61 @@ namespace CqrsSample.Inventory.CommandStack.Tests.Infrastructure
       Assert.IsNotNull(result);
       Assert.AreEqual(2, result.Count);
       CollectionAssert.AreEqual(events, result);
+    }
+
+    [Test]
+    public void GetEventsForAggregate_Reads_Events_From_Underlying_Event_Store()
+    {
+      // ARRANGE
+      var fixture = new Fixture();
+      fixture.Customize(new AutoMoqCustomization());
+
+      var events = fixture.CreateMany<Event>(2);
+
+      var streamMock = new Mock<IEventStream>();
+      streamMock
+        .Setup(m => m.CommittedEvents)
+        .Returns(() => events.Select(e => new EventMessage { Body = e }).ToList());
+
+      _storeMock
+        .Setup(m =>
+          m.OpenStream(
+            It.IsAny<string>(),
+            It.IsAny<string>(),
+            It.IsAny<int>(),
+            It.IsAny<int>()
+          )
+        )
+        .Returns(streamMock.Object);
+
+      var target = this.CreateTarget();
+
+      // ACT
+      var aggregateId = Guid.NewGuid();
+      _ = target.GetEventsForAggregate(aggregateId);
+
+      // ASSERT
+      _storeMock
+        .Verify(m =>
+          m.OpenStream(
+            It.IsAny<string>(),
+            It.IsAny<string>(),
+            It.IsAny<int>(),
+            It.IsAny<int>()
+          ),
+          Times.Once()
+        );
+
+      _storeMock
+        .Verify(m =>
+          m.OpenStream(
+            It.IsAny<string>(),
+            aggregateId.ToString(),
+            0,
+            int.MaxValue
+          ),
+          Times.Once()
+        );
     }
 
     private EventStore CreateTarget()
